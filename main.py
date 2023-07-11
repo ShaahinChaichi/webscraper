e423ef7ba3fab2c4b420f0dd7d4cc49c9e08ecd5
Shaahin Chaichi <shaahin.chaichi@gmail.com>
2023-07-11 22:04:24 +0330

    Points,text, URL scraping finished

--- main.py
+++ main.py
@@ -6,12 +6,17 @@ yc_web_page = response.text
 
 soup = BeautifulSoup(yc_web_page, "html.parser")
 
-article_tag = soup.find(name="a", rel="noreferrer")
-article_text = article_tag.getText()
-article_link = article_tag.get("href")
-article_upvote = soup.find(name="span", class_="score").getText()
+article_tags = soup.find_all(name="a", rel="noreferrer")
+article_texts = []
+article_link = []
 
-print(article_text)
+for article in article_tags:
+    article_texts.append(article.getText())
+    article_link.append(article.getLink())
+
+article_upvotes = soup.find_all(name="span", class_="score")
+
+print(article_texts)
 print(article_link)
-print(article_upvote)
+print(article_upvotes)
 
